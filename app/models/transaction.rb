354ce5744dc1a872baea1b6db3681ae8abc0ee7c
chrisdrydenaltmetric <chris@altmetric.com>
2023-09-29 13:25:36 +0100

    Add turboframes to transaction list and specific transactions

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,8 @@
 class Transaction < ApplicationRecord
   validates :amount, numericality: { message: "must be a number" }, presence: true
   validates :description, presence: true
+
+  after_create_commit { broadcast_prepend_to "transactions" }
+  after_update_commit { broadcast_replace_to "transactions" }
+  after_destroy_commit { broadcast_remove_to "transactions" }
 end
